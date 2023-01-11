647bf83247b5c7423cb15cb9c46ba4b96359471d
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2023-01-11 20:01:41 +0100

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {Hydration} from "react-async-states"
+import {myBackendContext} from "./becontext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Hydration context={myBackendContext}><Component {...pageProps} /></Hydration>
 }
